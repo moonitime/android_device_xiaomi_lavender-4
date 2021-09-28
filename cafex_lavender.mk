4e14a725b15b10c3e6250e165aaec6c37509d086
wHo-EM-i <ehteshammalik18998@gmail.com>
2021-09-28 23:33:34 +0530

    lavender: bringup cafex

--- cafex_lavender.mk
+++ cafex_lavender.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The AEX Project
+# Copyright (C) 2019 The CAFEX Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,19 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common AEX Project stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common CAFEX stuff
+$(call inherit-product, vendor/extended/common.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := cafex_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
 PRODUCT_MANUFACTURER := Xiaomi
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
