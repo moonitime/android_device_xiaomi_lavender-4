xenxynon/xd_dt

lavender: Use latest clang version for kernel compiler
Signed-off-by: ImPrashantt <prashant33968@gmail.com>

BoardConfig.mk
@@ -48,6 +48,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 TARGET_KERNEL_CONFIG := lavender-perf_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_CLANG_VERSION := latest
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm660

moonitime/android_device_xiaomi_lavender-4

BoardConfig.mk
@@ -51,6 +51,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 TARGET_KERNEL_CONFIG := lavender-perf_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_CLANG_VERSION := r416183b1
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm660