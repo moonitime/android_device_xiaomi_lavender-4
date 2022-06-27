b801986caa223af5d8e50d11d1c84417f86f5b46
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2022-06-28 00:45:59 +0530

    lavender: SKIP_API_CHECKS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,7 @@ TARGET_OTA_ASSERT_DEVICE := lavender
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME :=sdm660
 TARGET_NO_BOOTLOADER := true
+SKIP_API_CHECKS := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 loop.max_part=7
